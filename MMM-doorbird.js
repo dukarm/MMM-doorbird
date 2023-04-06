813f603a14990d1a51ae1212978ea1ab7de17ef8
dukarm <script@upgrade.com>
2023-04-06 19:50:37 +0200

    module fonctionnel with video and door opening

--- MMM-doorbird.js
+++ MMM-doorbird.js
@@ -1,86 +1,174 @@
+
 Module.register("MMM-doorbird", {
   defaults: {
     doorbirdUrl: "",
+    doorbirdHost: "",
     doorbirdUsername: "",
     doorbirdPassword: "",
+    doorbirdRelay: "1",
     magicMirrorIP: "",
     magicMirrorPort: "",
+    modulePort: "",
     displayDuration: 40 * 1000,
     width: 640,
     height: 480
   },
 
+
   start: function () {
-    this.visible = false;
-    this.videoUrl = "";
     this.sendSocketNotification("CONFIG", this.config);
   },
 
   getStyles: function () {
-    return ["MMM-doorbird.css"];
+     return ["MMM-doorbird.css"];
   },
 
+
   getDom: function () {
-    var wrapper = document.createElement("div");
-    wrapper.className = "MMM-doorbird";
+    const wrapper = document.createElement("div");
+    wrapper.className = "video-container";
 
-    if (this.visible) {
-      var video = document.createElement("video");
-      video.setAttribute("src", this.videoUrl);
-      video.setAttribute("autoplay", "autoplay");
-      video.setAttribute("width", this.config.width);
-      video.setAttribute("height", this.config.height);
-      wrapper.appendChild(video);
+      if (this.config.doorbirdUrl && this.config.doorbirdHost && this.config.doorbirdUsername && this.config.doorbirdPassword && this.config.magicMirrorIP && this.config.magicMirrorPort && this.config.modulePort) {
+       this.container = wrapper;
 
-      // Add buttons for microphone and door control
-      var buttonContainer = document.createElement("div");
-      buttonContainer.className = "button-container";
+       var buttonContainer = document.createElement("div");
+       buttonContainer.className = "button-container";
+       buttonContainer.style.display = "none"; // Hide the buttons by default
 
-      var micButton = document.createElement("button");
-      micButton.className = "button green";
-      micButton.innerHTML = "📞";
-      micButton.addEventListener("click", () => {
-        this.handleMicButtonClick(micButton);
-      });
-      buttonContainer.appendChild(micButton);
+       var closeButton = document.createElement("button");
+       closeButton.className = "mmm-doorbird-close";
+       closeButton.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+       closeButton.addEventListener("click", () => {
+         this.closebuttonevent();
+       });
+       buttonContainer.appendChild(closeButton); // Append closeButton to  buttonContainer
 
-      var doorButton = document.createElement("button");
-      doorButton.className = "button";
-      doorButton.innerHTML = "🔓";
-      doorButton.addEventListener("click", () => {
-        this.sendSocketNotification("ACTIVATE_DOOR_OPENER");
-      });
-      buttonContainer.appendChild(doorButton);
+       var micButton = document.createElement("button");
+       micButton.className = "button green";
+       micButton.innerHTML = "<i class='fas fa-phone'></i>";
+       micButton.addEventListener("click", () => {
+         this.handleMicButtonClick(micButton);
+       });
+       buttonContainer.appendChild(micButton);
 
-      wrapper.appendChild(buttonContainer);
-    }
+       var doorButton = document.createElement("button");
+       doorButton.className = "button";
+       doorButton.setAttribute("data-locked", "true");
+       doorButton.innerHTML = "<i class='fas fa-lock'></i>";
+       doorButton.addEventListener("click", () => {
+	 this.handleDoorButtonClick(doorButton);
+         this.sendSocketNotification("ACTIVATE_DOOR_OPENER");
+         });
+       buttonContainer.appendChild(doorButton);
+
+       wrapper.appendChild(buttonContainer);
+       } else {
+         wrapper.innerHTML = "Doorbird configuration is incomplete";
+       }
 
     return wrapper;
   },
 
-  handleMicButtonClick: function (button) {
-    if (button.classList.contains("green")) {
-      button.innerHTML = "📞";
-      button.classList.remove("green");
-      button.classList.add("red");
-      this.sendSocketNotification("START_MIC");
-    } else {
-      button.innerHTML = "📞";
-      button.classList.remove("red");
-      button.classList.add("green");
-      this.sendSocketNotification("STOP_MIC");
+
+  closebuttonevent: function () {
+    const videoContainer = document.querySelector(".video-container");
+    const video = document.querySelector(".doorbird-video");
+    const buttonContainer = document.querySelector(".button-container");
+
+    video.src = ""; // Set the video src to an empty string to stop the request
+    videoContainer.removeChild(video); // Remove the video element
+    buttonContainer.style.display = "none"; // Hide the buttons
+  },
+
+  handleDoorButtonClick: function (button) {
+    const locked = button.getAttribute("data-locked") === "true";
+    if (locked) {
+      button.innerHTML = "<i class='fas fa-unlock'></i>";
+      button.setAttribute("data-locked", "false");
+      this.sendSocketNotification("ACTIVATE_DOOR_OPENER");
+
+      // Rétablir l'icône du cadenas verrouillé après 10 secondes
+      setTimeout(() => {
+        button.innerHTML = "<i class='fas fa-lock'></i>";
+        button.setAttribute("data-locked", "true");
+      }, 10 * 1000);
     }
   },
 
+  handleMicButtonClick: function (button) {
+    if (button.classList.contains("green")) {
+      button.innerHTML = "<i class='fas fa-phone-slash'></i>";
+      button.classList.remove("green");
+      button.classList.add("red");
+      this.sendSocketNotification("START_AUDIO");
+    } else {
+      button.innerHTML = "<i class='fas fa-phone-alt'></i>";
+      button.classList.remove("red");
+      button.classList.add("green");
+      this.sendSocketNotification("STOP_AUDIO");
+    }
+  },
+
+  showButtons: function () {
+    const buttonContainer = document.querySelector(".button-container");
+    if (buttonContainer) {
+      buttonContainer.style.display = "block";
+    }
+  },
+
+  hideButtons: function () {
+    const buttonContainer = document.querySelector(".button-container");
+    if (buttonContainer) {
+      buttonContainer.style.display = "none";
+    }
+  },
+
+  showVideo: function () {
+    const doorbirdWrapper = document.getElementById("doorbird-wrapper");
+
+    const video = document.createElement("img");
+    this.sendNotification("SCREEN_WAKEUP");
+    console.log('MMM-doorbird: Screen wakeup notification send.');
+    video.id = "doorbird-video";
+    video.src = this.videoUrl;
+    video.style.width = this.config.width;
+    video.style.height = this.config.height;
+    doorbirdWrapper.insertBefore(video, doorbirdWrapper.firstChild);
+  },
+
+  hideVideo: function () {
+    const doorbirdWrapper = document.getElementById("doorbird-wrapper");
+    const videoElement = document.getElementById("doorbird-video");
+    if (videoElement) {
+      doorbirdWrapper.removeChild(videoElement);
+    }
+  },
+
+
   socketNotificationReceived: function (notification, payload) {
+    console.log("Socket notification received:", notification, payload);
     if (notification === "SHOW_VIDEO") {
-      this.videoUrl = payload;
-      this.visible = true;
-      this.updateDom();
+      this.videoUrl = `${this.config.doorbirdUrl}/bha-api/video.cgi?http-user=${encodeURIComponent(this.config.doorbirdUsername)}&http-password=${encodeURIComponent(this.config.doorbirdPassword)}`
+
+      const videoContainer = document.querySelector(".video-container");
+      const video = document.createElement("img");
+      video.src = this.videoUrl;
+      video.style.width = this.config.width;
+      video.style.height = this.config.height;
+      video.className = "doorbird-video";
+      videoContainer.appendChild(video);
+
+      const buttonContainer = document.querySelector(".button-container");
+      buttonContainer.style.display = "block"; // Show the buttons
+
       setTimeout(() => {
-        this.visible = false;
-        this.updateDom();
-      }, this.config.displayDuration);
+        video.src = ""; // Set the video src to an empty string to stop the request
+        videoContainer.removeChild(video); // Remove the video element
+        buttonContainer.style.display = "none"; // Hide the buttons
+       }, this.config.displayDuration);
     }
   }
+
+
+
 });
--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,9 @@
 const NodeHelper = require("node_helper");
 const request = require("request-promise");
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
 
 module.exports = NodeHelper.create({
   start: function () {
@@ -10,57 +14,89 @@ module.exports = NodeHelper.create({
     if (notification === "CONFIG") {
       this.config = payload;
       this.setupDoorbirdEvents();
+      this.createExpressServer();
+    } else if (notification === "ACTIVATE_DOOR_OPENER") {
+      this.activateDoorOpener();
     } else if (notification === "START_MIC") {
       this.startMicrophone();
     } else if (notification === "STOP_MIC") {
-      this.stopMicrophone();
-    } else if (notification === "ACTIVATE_DOOR_OPENER") {
-      this.activateDoorOpener();
-    }
+      this.stopMicrophone(); }
   },
+
+  createExpressServer: function () {
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+
+    app.get('/doorbell', (req, res) => {
+      const doorbirdModule = 'MMM-doorbird'; // Le nom de votre module Doorbird
+      const payload = { lockString: doorbirdModule };
+      const notification = 'DOORBELL_ACTIVATED';
+      this.sendSocketNotification("SHOW_VIDEO");
+      console.log("SHOW_VIDEO socket notification sent");
+      console.log("SCREEN_WAKEUP notification sent");
+
+      res.send('Doorbell activated!');
+    });
+
+    app.post("/doorbird/notification", (req, res) => {
+      console.log("Doorbird ring notification received");
+      this.sendSocketNotification("SHOW_VIDEO");
+      console.log("SHOW_VIDEO socket notification sent");
+      res.sendStatus(200);
+    });
+
+    const port = this.config.modulePort;
+    app.listen(port, () => {
+      console.log(`MMM-doorbird listening on port ${port}`);
+    });
+  },
+
+  startMicrophone: function () {},
+
+  stopMicrophone: function () {},
+
 
   setupDoorbirdEvents: function () {
     // Configure Doorbird to notify when someone rings the bell
-    const url = `${this.config.doorbirdUrl}/bha-api/notification.cgi?url=http://${this.config.magicMirrorIP}:${this.config.magicMirrorPort}/doorbird/notification&event=doorbell&subscribe=1`;
+    const url = `${this.config.doorbirdUrl}/bha-api/notification.cgi?url=http://${this.config.magicMirrorIP}:${this.config.modulePort}/doorbell&event=doorbell&subscribe=1`;
     const options = {
       uri: url,
       auth: {
         user: this.config.doorbirdUsername,
-      pass: this.config.doorbirdPassword,
-      sendImmediately: false
-    },
-    method: "GET"
-  };
-  request(options).then(() => {
-    console.log("Doorbird notifications configured");
-  }).catch((error) => {
-    console.error("Failed to configure Doorbird notifications", error);
-  });
-},
+        pass: this.config.doorbirdPassword,
+        sendImmediately: false
+       },
+      method: "GET"
+    };
+    request(options).then(() => {
+      console.log("Doorbird notifications configured");
+    }).catch((error) => {
+      console.error("Failed to configure Doorbird notifications", error);
+    });
+  },
 
-startMicrophone: function () {
-  // Implement microphone streaming to Doorbird
-},
 
-stopMicrophone: function () {
-  // Stop microphone streaming to Doorbird
-},
-
-activateDoorOpener: function () {
-  const url = `${this.config.doorbirdUrl}/bha-api/open-door.cgi?r=1`;
-  const options = {
-    uri: url,
-    auth: {
-      user: this.config.doorbirdUsername,
-      pass: this.config.doorbirdPassword,
-      sendImmediately: false
-    },
-    method: "GET"
-  };
-  request(options).then(() => {
-    console.log("Door opener activated");
-  }).catch((error) => {
-    console.error("Failed to activate door opener", error);
-  });
-}
+  activateDoorOpener: function () {
+    const url = `${this.config.doorbirdUrl}/bha-api/open-door.cgi?r=${this.config.doorbirdRelay}`;
+    const options = {
+      uri: url,
+      auth: {
+        user: this.config.doorbirdUsername,
+        pass: this.config.doorbirdPassword,
+        sendImmediately: false
+      },
+       method: "GET"
+    };
+      request(options).then(() => {
+      console.log("Door opener activated");
+      }).catch((error) => {
+      console.error("Failed to activate door opener", error);
+    });
+  }
 });
